518cd58e49b3f8c894ade7a0036042e9628792d2
sspos <78341160+sspos@users.noreply.github.com>
2024-10-04 03:26:25 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,9 @@
 const auth = firebase.auth();
 
+
+// نموذج إنشاء الحساب
+
+
 const signupForm = document.getElementById('signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -21,5 +25,44 @@ signupForm.addEventListener('submit', (e) => {
     })
     .catch((error) => {
       alert("حدث خطأ أثناء إنشاء الحساب: " + error.message);
+    });
+});
+
+
+
+
+// نموذج تسجيل الدخول
+
+
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const userId = userCredential.user.uid;
+
+      // استرجاع بيانات المستخدم من Firestore
+      db.collection('users').doc(userId).get().then((doc) => {
+        if (doc.exists) {
+          const userData = doc.data();
+          if (userData.isAdmin) {
+            // المستخدم هو أدمن، إعادة التوجيه إلى صفحة نشر الأخبار
+            alert("مرحباً بك يا أدمن!");
+            window.location.href = "admin.html"; // توجيه الأدمن إلى صفحة الإدارة
+          } else {
+            // المستخدم ليس أدمن، توجيهه للصفحة الرئيسية
+            alert("تم تسجيل الدخول بنجاح!");
+            window.location.href = "index.html";
+          }
+        } else {
+          alert("لا يوجد بيانات لهذا المستخدم.");
+        }
+      });
+    })
+    .catch((error) => {
+      alert("حدث خطأ أثناء تسجيل الدخول: " + error.message);
     });
 });
