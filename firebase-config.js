98faec642266c23fd1d82a01dd804e770cc43bf4
sspos <78341160+sspos@users.noreply.github.com>
2024-10-04 07:55:13 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,21 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDS99hQFXjGe-lxdj1J1KxGF9AS-PZpemg",
-  authDomain: "sposnews-321f7.firebaseapp.com",
-  projectId: "sposnews-321f7",
-  storageBucket: "sposnews-321f7.appspot.com",
-  messagingSenderId: "85605938688",
-  appId: "1:85605938688:web:f59b2f8bd190a9a56d4ae5",
-  measurementId: "G-DDCGG6ZNK4"
+// firebase-config.js
+
+// إعداد Firebase
+var firebaseConfig = {
+
+  apiKey: "AIzaSyAYm33ORopfeFBW22gdnLU71Yq2W62UN58",
+  authDomain: "spos-sport.firebaseapp.com",
+  databaseURL: "https://spos-sport-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "spos-sport",
+  storageBucket: "spos-sport.appspot.com",
+  messagingSenderId: "755322200260",
+  appId: "1:755322200260:web:b48a65bdb3528c5a28a991",
+  measurementId: "G-ZKMWF7WKMD"
+
 };
 
-// Initialize Firebase
+// تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Firestore reference
-const db = firebase.firestore();
+// تهيئة Firestore (إذا كنت تستخدمه)
+var db = firebase.firestore();
