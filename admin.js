709925fd50c0fb5a08f018dde241bb645cbfbfe5
sspos <78341160+sspos@users.noreply.github.com>
2024-10-04 08:03:20 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,22 @@
 const newsForm = document.getElementById('news-form');
 newsForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+
+
+  firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    db.collection('users').doc(user.uid).get().then((doc) => {
+      if (!doc.exists || !doc.data().isAdmin) {
+        alert("ليس لديك الصلاحية لدخول هذه الصفحة.");
+        window.location.href = "index.html"; // إعادة توجيه المستخدمين العاديين إلى الصفحة الرئيسية
+      }
+    });
+  } else {
+    alert("يجب عليك تسجيل الدخول.");
+    window.location.href = "login.html"; // إعادة توجيه المستخدمين غير المسجلين إلى صفحة تسجيل الدخول
+  }
+});
   
   const title = document.getElementById('title').value;
   const content = document.getElementById('content').value;
